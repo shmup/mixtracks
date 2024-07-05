a66e50cb678868029fa14e850a53e0227f932b56
Jared Miller <jared@smell.flowers>
2024-07-05 14:04:09 -0400

    Display ordered list of all mp3s in tracks

--- public/index.php
+++ public/index.php
@@ -1,3 +1,23 @@
 <?php
-phpinfo();
+$directory = "tracks";
+
+function removeLeadingTrackNumber($track)
+{
+    return preg_replace("/^\d{2} - /", "", $track);
+}
+
+if (is_dir($directory)) {
+    $files = scandir($directory);
+    echo "<ol>";
+    foreach ($files as $file) {
+        if ($file !== "." && $file !== "..") {
+            echo "<li>" .
+                htmlspecialchars(removeLeadingTrackNumber($file)) .
+                "</li>";
+        }
+    }
+    echo "</ol>";
+} else {
+    echo "no tracks";
+}
 ?>
