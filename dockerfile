d4232cd0cf60d9cdee803d18721e77bf97b7e607
Jared Miller <jared@smell.flowers>
2024-07-05 14:25:49 -0400

    Add php extension getID3 and output tags

--- dockerfile
+++ dockerfile
@@ -1,6 +1,14 @@
 FROM php:8.1-cli
 
-RUN apt-get update && apt-get install -y
+RUN apt-get update && apt-get install -y \
+    libonig-dev \
+    unzip \
+    && docker-php-ext-install mbstring
+
+RUN curl -L https://github.com/JamesHeinrich/getID3/archive/master.zip -o getid3.zip \
+    && unzip getid3.zip -d /usr/src/php/ \
+    && mv /usr/src/php/getID3-master /usr/src/php/getID3 \
+    && rm getid3.zip
 
 WORKDIR /var/www/html
 
--- public/index.php
+++ public/index.php
@@ -1,4 +1,6 @@
 <?php
+require_once "/usr/src/php/getID3/getid3/getid3.php";
+
 $directory = "tracks";
 
 function removeLeadingTrackNumber($track)
@@ -6,13 +8,38 @@ function removeLeadingTrackNumber($track)
     return preg_replace("/^\d{2} - /", "", $track);
 }
 
+function getTrackMetadata($filePath)
+{
+    $getID3 = new getID3();
+    $fileInfo = $getID3->analyze($filePath);
+    getid3_lib::CopyTagsToComments($fileInfo);
+
+    return [
+        "title" => $fileInfo["comments_html"]["title"][0] ?? "Unknown Title",
+        "artist" => $fileInfo["comments_html"]["artist"][0] ?? "Unknown Artist",
+        "album" => $fileInfo["comments_html"]["album"][0] ?? "Unknown Album",
+        "year" => $fileInfo["comments_html"]["year"][0] ?? "Unknown Year",
+    ];
+}
+
 if (is_dir($directory)) {
     $files = scandir($directory);
     echo "<ol>";
     foreach ($files as $file) {
         if ($file !== "." && $file !== "..") {
+            $filePath = $directory . DIRECTORY_SEPARATOR . $file;
+            $metadata = getTrackMetadata($filePath);
             echo "<li>" .
                 htmlspecialchars(removeLeadingTrackNumber($file)) .
+                " - " .
+                htmlspecialchars($metadata["title"]) .
+                " by " .
+                htmlspecialchars($metadata["artist"]) .
+                " (" .
+                htmlspecialchars($metadata["album"]) .
+                ", " .
+                htmlspecialchars($metadata["year"]) .
+                ")" .
                 "</li>";
         }
     }
